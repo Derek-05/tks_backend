3ab2f6694816ad36e227bbdcec6eda17b6448301
Derek <dereksanchezrosas@hotmail.com>
2024-02-07 20:46:33 -0400

    I added origin to cors

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded ({
     extended: true
 }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({origin: 'http://localhost:3000'}));
 app.use(express.json());
 
 
